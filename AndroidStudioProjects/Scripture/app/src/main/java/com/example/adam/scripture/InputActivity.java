5d932e92bcca64bcaed96a2eec0e8e45c5c1b3d3
Adam <cam10017@byui.edu>
2015-10-17 22:51:23 -0600

    Some New Scripture Stuff

---
+++ AndroidStudioProjects/Scripture/app/src/main/java/com/example/adam/scripture/InputActivity.java
@@ -0,0 +1,70 @@
+package com.example.adam.scripture;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class InputActivity extends AppCompatActivity {
+
+    public void shareScripture(View view) {
+        Intent sendIntent = new Intent(InputActivity.this, ShareActivity.class);
+        Bundle bundle = new Bundle();
+
+        //put data from text boxes into an intent bundle
+        TextView t = (TextView) findViewById(R.id.bookText);
+        bundle.putString("book", t.getText().toString());
+        t = (TextView) findViewById(R.id.chapterText);
+        bundle.putString("chapter", t.getText().toString());
+        t = (TextView) findViewById(R.id.verseText);
+        bundle.putString("verse", t.getText().toString());
+
+        sendIntent.putExtras(bundle);
+        startActivity(sendIntent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_input, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
